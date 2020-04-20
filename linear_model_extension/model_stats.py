78a1e3eb3014d4a25181631cca5618e904204a32
katherine501 <katherineedgley@gmail.com>
2020-04-20 09:35:12 +0100

    docs: comment functions in ModelStats and RegressionStats

--- linear_model_extension/model_stats.py
+++ linear_model_extension/model_stats.py
@@ -5,6 +5,21 @@ import numpy as np
 import pandas as pd
 
 class ModelStats:
+    '''
+    General class (not to be called outside of RegressionStats) for classes
+    that generate model statistics, like RegressionStats
+    Input:
+        fitted_model - a scikit-learn LinearRegression fitted model
+        X  - feature matrix used to fit the sklearn model, can be 
+                numpy array or pandas df
+        y - target array used to fit the sklearn model, 
+                np array or pandas series
+        colnames - default None, only for supplying a list of the column
+                    names when inputting X as a numpy array, which
+                    does not include the names of the columns
+                    If colnames=None and X is numpy array, 
+                    will name the variables in order.
+    '''
     def __init__(self, fitted_model, X, y, colnames = None):
         self.X = np.asarray(X)
         self.y = np.asarray(y)
